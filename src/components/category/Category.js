f129ac630122b7c1d1bd7013e61b84146e506735
Muhammed DAGDELEN <muhammedemindagdelenn@gmail.com>
2022-02-23 22:02:48 +0300

    activeClass/category

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 import "./category.scss";
 function Category({ categories, filterItems }) {
+  const [currentCategory, setCurrenCategory] = useState(1);
   return (
     <div className="categories">
       <h1>Categories</h1>
       <div className="btn-container">
         {categories.map((category, index) => {
           return (
+            <li className={currentCategory === index ? "active" : "none"} onClick={() => setCurrenCategory(index)}>
             <button
               type="button"
               key={index}
@@ -15,6 +17,7 @@ function Category({ categories, filterItems }) {
             >
               {category}
             </button>
+            </li>
           );
         })}
       </div>
